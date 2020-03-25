67d6e2e325e1314d896bd2a12e8cf4dc7d27c248
bhanukushwah <bhanupskofficial@gmail.com>
2020-03-25 20:39:38 +0530

    Added Search functionality

--- src/index.js
+++ src/index.js
@@ -2,26 +2,40 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_details';
 const API_KEY = 'AIzaSyCiDZvcLBPtj70s98qPBj4BgOLDhX-_qPY';
 
 class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos: [] }
-
-        YTSearch({key: API_KEY, term: 'surfboard'}, (videos) =>{
-            this.setState({videos});
-        })
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        }
+        this.videoSearch('Arijit Singh');
     } 
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) =>{
+            this.setState({ 
+                videos:videos,
+                selectedVideo: videos[0]
+             });
+        });
+    }
+
     render () {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
+                <VideoDetails video = {this.state.selectedVideo}/>
+                <VideoList 
+                onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos}/>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector('.container'));
